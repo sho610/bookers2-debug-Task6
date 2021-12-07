a5bba00fc4e46c9a12535f6d240f903f2ef5b174
sho610 <ec2-user@ip-172-31-54-218.ec2.internal>
2021-12-07 01:46:56 +0000

    [Task5]edit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get "home/about" => "homes#about"
+  get "/search", to: 'searchs#search'
 
   resources :users, only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
